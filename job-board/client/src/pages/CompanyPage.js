1a4ddaff33405688689cf3ca29e88de8bccdf457
Luiz Glomyer <glomyerjunior@hotmail.com>
2024-10-05 17:20:16 -0400

    feat: initial queries and integrations

--- job-board/client/src/pages/CompanyPage.js
+++ job-board/client/src/pages/CompanyPage.js
@@ -1,10 +1,19 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { companies } from '../lib/fake-data';
+import JobList from '../components/JobList';
+import { getCompany } from '../lib/graphql/queries';
 
 function CompanyPage() {
   const { companyId } = useParams();
+  const [company, setCompany] = useState();
+  useEffect(() => {
+    getCompany(companyId).then(setCompany);
+  }, [companyId]);
 
-  const company = companies.find((company) => company.id === companyId);
+  console.log('[CompanyPage] company:', company);
+  if (!company) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <h1 className="title">
@@ -13,6 +22,10 @@ function CompanyPage() {
       <div className="box">
         {company.description}
       </div>
+      <h2 className="title is-5">
+        Jobs at {company.name}
+      </h2>
+      <JobList jobs={company.jobs} />
     </div>
   );
 }
